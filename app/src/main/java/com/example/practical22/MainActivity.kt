279f02c901a648f11f6df16b1bb6c4374c270ca6
Acer <weiyangtan1999@gmail.com>
2020-03-11 23:40:19 +0800

    last step

--- app/src/main/java/com/example/practical22/MainActivity.kt
+++ app/src/main/java/com/example/practical22/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.practical22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun addNickname(view: View){
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+
+    }
+
 }
