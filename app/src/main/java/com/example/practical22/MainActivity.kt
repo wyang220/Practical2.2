643a0e93df4a7798a53411788606a7f950962da9
taruc <weiyangtan1999@gmail.com>
2020-03-12 11:44:11 +0800

    done

--- app/src/main/java/com/example/practical22/MainActivity.kt
+++ app/src/main/java/com/example/practical22/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.practical22
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -12,6 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val doneButton: Button = findViewById(R.id.done_button)
+        doneButton.setOnClickListener { addNickname(it) } //it == current value
+
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(it)
+        }
     }
 
     private fun addNickname(view: View){
@@ -27,6 +36,29 @@ class MainActivity : AppCompatActivity() {
             addNickname(it)
         }
 
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickname (view: View) {
+
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(it)
+
+            // Set the focus to the edit text.
+            editText.requestFocus()
+
+            // Show the keyboard.
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(editText, 0)
+        }
     }
 
 }
